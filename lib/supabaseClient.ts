13de67b85d1912b68c9da4562ec8828aba92a939
MOH NUR SALIM <43797082+salim234@users.noreply.github.com>
2025-11-14 10:53:19 +0700

    Update Supabase client URL and Anon Keyupdate database

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://cheryyyuptjbjdltfvph.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoZXJ5eXl1cHRqYmpkbHRmdnBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwMjU0NDksImV4cCI6MjA3ODYwMTQ0OX0.4rnbd5EzusLL0Tf0QP_d5sG3HakDjsODQ0vQOcB84yc';
+const supabaseUrl = 'https://tujpjgyuiauanvlvsygb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1anBqZ3l1aWF1YW52bHZzeWdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNTQzODYsImV4cCI6MjA3ODYzMDM4Nn0.s8ZeBBE9TOqHB4FKtK_Ucc643y2ocXNZhAWVWha_85I';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and Anon Key must be provided.');
